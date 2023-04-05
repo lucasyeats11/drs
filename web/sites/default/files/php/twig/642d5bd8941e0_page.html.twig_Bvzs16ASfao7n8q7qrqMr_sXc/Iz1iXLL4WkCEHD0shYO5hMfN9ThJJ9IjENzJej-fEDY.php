f9c8516cf995c897009e9b1ab1d13e95ea2c10ec
lucas <lucas@codewerks.co.uk>
2023-04-05 12:58:26 +0100

    adding footer

---
+++ web/sites/default/files/php/twig/642d5bd8941e0_page.html.twig_Bvzs16ASfao7n8q7qrqMr_sXc/Iz1iXLL4WkCEHD0shYO5hMfN9ThJJ9IjENzJej-fEDY.php
@@ -0,0 +1,286 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/deposit_return_theme/templates/layout/page.html.twig */
+class __TwigTemplate_748b2865f6011d408180ca718bff3c7e extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 46
+        $context["nav_classes"] = ((("navbar navbar-expand-lg" . (((        // line 47
+($context["b5_navbar_schema"] ?? null) != "none")) ? ((" navbar-" . $this->sandbox->ensureToStringAllowed(($context["b5_navbar_schema"] ?? null), 47, $this->source))) : (" "))) . (((        // line 48
+($context["b5_navbar_schema"] ?? null) != "none")) ? ((((($context["b5_navbar_schema"] ?? null) == "dark")) ? (" text-light") : (" text-dark"))) : (" "))) . (((        // line 49
+($context["b5_navbar_bg_schema"] ?? null) != "none")) ? ((" bg-" . $this->sandbox->ensureToStringAllowed(($context["b5_navbar_bg_schema"] ?? null), 49, $this->source))) : (" ")));
+        // line 51
+        echo "
+<header>
+  ";
+        // line 53
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "header", [], "any", false, false, true, 53), 53, $this->source), "html", null, true);
+        echo "
+
+  ";
+        // line 55
+        if (((twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "nav_branding", [], "any", false, false, true, 55) || twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "nav_main", [], "any", false, false, true, 55)) || twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "nav_additional", [], "any", false, false, true, 55))) {
+            // line 56
+            echo "  <nav class=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["nav_classes"] ?? null), 56, $this->source), "html", null, true);
+            echo "\">
+    <div class=\"";
+            // line 57
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["b5_top_container"] ?? null), 57, $this->source), "html", null, true);
+            echo " d-flex\">
+      ";
+            // line 58
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "nav_branding", [], "any", false, false, true, 58), 58, $this->source), "html", null, true);
+            echo "
+
+      <button class=\"navbar-toggler collapsed\" type=\"button\" data-bs-toggle=\"collapse\"
+              data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\"
+              aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+        <span class=\"navbar-toggler-icon\"></span>
+      </button>
+
+      <div class=\"collapse navbar-collapse justify-content-md-end\" id=\"navbarSupportedContent\">
+        ";
+            // line 67
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "nav_main", [], "any", false, false, true, 67), 67, $this->source), "html", null, true);
+            echo "
+        ";
+            // line 68
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "nav_additional", [], "any", false, false, true, 68), 68, $this->source), "html", null, true);
+            echo "
+      </div>
+    </div>
+  </nav>
+  ";
+        }
+        // line 73
+        echo "
+</header>
+
+<main role=\"main\">
+  <a id=\"main-content\" tabindex=\"-1\"></a>";
+        // line 78
+        echo "
+  <div class=\"";
+        // line 79
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["b5_top_container"] ?? null), 79, $this->source), "html", null, true);
+        echo "\">
+    ";
+        // line 80
+        if (twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "breadcrumb", [], "any", false, false, true, 80)) {
+            // line 81
+            echo "      ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "breadcrumb", [], "any", false, false, true, 81), 81, $this->source), "html", null, true);
+            echo "
+    ";
+        }
+        // line 83
+        echo "    <div class=\"row g-0\">
+      <div class=\"order-1 order-lg-2 ";
+        // line 84
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content_classes"] ?? null), 84, $this->source), "html", null, true);
+        echo "\">
+        ";
+        // line 85
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "content", [], "any", false, false, true, 85), 85, $this->source), "html", null, true);
+        echo "
+      </div>
+    </div>
+  </div>
+
+</main>
+
+<footer class=\"mt-auto\">
+  <div class=\"";
+        // line 93
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["b5_top_container"] ?? null), 93, $this->source), "html", null, true);
+        echo "\">
+    ";
+        // line 94
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "first_footer", [], "any", false, false, true, 94), 94, $this->source), "html", null, true);
+        echo "
+    ";
+        // line 95
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "footer", [], "any", false, false, true, 95), 95, $this->source), "html", null, true);
+        echo "
+  </div>
+</footer>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/deposit_return_theme/templates/layout/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  135 => 95,  131 => 94,  127 => 93,  116 => 85,  112 => 84,  109 => 83,  103 => 81,  101 => 80,  97 => 79,  94 => 78,  88 => 73,  80 => 68,  76 => 67,  64 => 58,  60 => 57,  55 => 56,  53 => 55,  48 => 53,  44 => 51,  42 => 49,  41 => 48,  40 => 47,  39 => 46,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override to display a single page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.html.twig template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - base_path: The base URL path of the Drupal installation. Will usually be
+ *   \"/\" unless you have installed Drupal in a sub-directory.
+ * - is_front: A flag indicating if the current page is the front page.
+ * - logged_in: A flag indicating if the user is registered and signed in.
+ * - is_admin: A flag indicating if the user has permission to access
+ *   administration pages.
+ *
+ * Site identity:
+ * - front_page: The URL of the front page. Use this instead of base_path when
+ *   linking to the front page. This includes the language domain or prefix.
+ *
+ * Page content (in order of occurrence in the default page.html.twig):
+ * - node: Fully loaded node, if there is an automatically-loaded node
+ *   associated with the page and the node ID is the second argument in the
+ *   page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - page.header: Items for the header region.
+ * - page.primary_menu: Items for the primary menu region.
+ * - page.secondary_menu: Items for the secondary menu region.
+ * - page.highlighted: Items for the highlighted content region.
+ * - page.help: Dynamic help text, mostly for admin pages.
+ * - page.content: The main content of the current page.
+ * - page.sidebar_first: Items for the first sidebar.
+ * - page.sidebar_second: Items for the second sidebar.
+ * - page.footer: Items for the footer region.
+ * - page.breadcrumb: Items for the breadcrumb region.
+ *
+ * @see template_preprocess_page()
+ * @see html.html.twig
+ */
+#}
+{%
+set nav_classes = 'navbar navbar-expand-lg' ~
+  (b5_navbar_schema != 'none' ? \" navbar-#{b5_navbar_schema}\" : ' ') ~
+  (b5_navbar_schema != 'none' ? (b5_navbar_schema == 'dark' ? ' text-light' : ' text-dark' ) : ' ') ~
+  (b5_navbar_bg_schema != 'none' ? \" bg-#{b5_navbar_bg_schema}\" : ' ')
+%}
+
+<header>
+  {{ page.header }}
+
+  {% if page.nav_branding or page.nav_main or page.nav_additional %}
+  <nav class=\"{{ nav_classes }}\">
+    <div class=\"{{ b5_top_container }} d-flex\">
+      {{ page.nav_branding }}
+
+      <button class=\"navbar-toggler collapsed\" type=\"button\" data-bs-toggle=\"collapse\"
+              data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\"
+              aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+        <span class=\"navbar-toggler-icon\"></span>
+      </button>
+
+      <div class=\"collapse navbar-collapse justify-content-md-end\" id=\"navbarSupportedContent\">
+        {{ page.nav_main }}
+        {{ page.nav_additional }}
+      </div>
+    </div>
+  </nav>
+  {% endif %}
+
+</header>
+
+<main role=\"main\">
+  <a id=\"main-content\" tabindex=\"-1\"></a>{# link is in html.html.twig #}
+
+  <div class=\"{{ b5_top_container }}\">
+    {% if page.breadcrumb %}
+      {{ page.breadcrumb }}
+    {% endif %}
+    <div class=\"row g-0\">
+      <div class=\"order-1 order-lg-2 {{ content_classes }}\">
+        {{ page.content }}
+      </div>
+    </div>
+  </div>
+
+</main>
+
+<footer class=\"mt-auto\">
+  <div class=\"{{ b5_top_container }}\">
+    {{ page.first_footer }}
+    {{ page.footer }}
+  </div>
+</footer>
+", "themes/custom/deposit_return_theme/templates/layout/page.html.twig", "/var/www/html/web/themes/custom/deposit_return_theme/templates/layout/page.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 46, "if" => 55);
+        static $filters = array("escape" => 53);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
