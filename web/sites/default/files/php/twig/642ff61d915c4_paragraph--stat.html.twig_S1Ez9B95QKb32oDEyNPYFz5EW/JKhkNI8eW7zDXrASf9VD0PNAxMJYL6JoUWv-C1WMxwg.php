714fa17501e4b2e8ccc7947f22e33362027f9c15
lucas <lucas@codewerks.co.uk>
2023-04-07 11:55:20 +0100

    adding stats

---
+++ web/sites/default/files/php/twig/642ff61d915c4_paragraph--stat.html.twig_S1Ez9B95QKb32oDEyNPYFz5EW/JKhkNI8eW7zDXrASf9VD0PNAxMJYL6JoUWv-C1WMxwg.php
@@ -0,0 +1,108 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/deposit_return_theme/templates/paragraphs/paragraph--stat.html.twig */
+class __TwigTemplate_d2cea44e2b7036d3a21d3e7d214f7496 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo " <div class=\"stat\">
+  ";
+        // line 2
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_image_upload", [], "any", false, false, true, 2), 2, $this->source), "html", null, true);
+        echo "
+  <h2>";
+        // line 3
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_title", [], "any", false, false, true, 3), 3, $this->source), "html", null, true);
+        echo "</h2>
+  ";
+        // line 4
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_text", [], "any", false, false, true, 4), 4, $this->source), "html", null, true);
+        echo "
+ </div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/deposit_return_theme/templates/paragraphs/paragraph--stat.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  50 => 4,  46 => 3,  42 => 2,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source(" <div class=\"stat\">
+  {{ content.field_image_upload }}
+  <h2>{{ content.field_title }}</h2>
+  {{ content.field_text }}
+ </div>
+", "themes/custom/deposit_return_theme/templates/paragraphs/paragraph--stat.html.twig", "/var/www/html/web/themes/custom/deposit_return_theme/templates/paragraphs/paragraph--stat.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 2);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
