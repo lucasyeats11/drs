f25c9f4035f1cb0fbef2a6849897ce3dcf9ff6ad
lucas <lucas@codewerks.co.uk>
2023-04-10 12:46:54 +0100

    getting background image working for banners

---
+++ web/sites/default/files/php/twig/6433f6b8f2853_paragraph--cards.html.twi_wQfytkde7yakvsG9IMy_N1G2O/sgIPjzUmyFBrsfaBBfeooBwzfl2UUzTE1hpy-cnOiK0.php
@@ -0,0 +1,144 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/deposit_return_theme/templates/paragraphs/paragraph--cards.html.twig */
+class __TwigTemplate_940264ea546824d2ce3357b61ab816e6 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'paragraph' => [$this, 'block_paragraph'],
+            'content' => [$this, 'block_content'],
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        $context["classes"] = [0 => "paragraph", 1 => ("paragraph--type--" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source,         // line 3
+($context["paragraph"] ?? null), "bundle", [], "any", false, false, true, 3), 3, $this->source))), 2 => ((        // line 4
+($context["view_mode"] ?? null)) ? (("paragraph--view-mode--" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["view_mode"] ?? null), 4, $this->source)))) : ("")), 3 => (( !twig_get_attribute($this->env, $this->source,         // line 5
+($context["paragraph"] ?? null), "isPublished", [], "method", false, false, true, 5)) ? ("paragraph--unpublished") : (""))];
+        // line 7
+        $this->displayBlock('paragraph', $context, $blocks);
+    }
+
+    public function block_paragraph($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 8
+        echo "  <div";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 8), 8, $this->source), "html", null, true);
+        echo ">
+    ";
+        // line 9
+        $this->displayBlock('content', $context, $blocks);
+        // line 16
+        echo "  </div>
+";
+    }
+
+    // line 9
+    public function block_content($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 10
+        echo "      <div class=\"cards\">
+        <div class=\"container\">
+          ";
+        // line 12
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_add_card", [], "any", false, false, true, 12), 12, $this->source), "html", null, true);
+        echo "
+        </div>
+      </div>
+    ";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/deposit_return_theme/templates/paragraphs/paragraph--cards.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  73 => 12,  69 => 10,  65 => 9,  60 => 16,  58 => 9,  53 => 8,  46 => 7,  44 => 5,  43 => 4,  42 => 3,  41 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% set classes = [
+  'paragraph',
+  'paragraph--type--' ~ paragraph.bundle|clean_class,
+  view_mode ? 'paragraph--view-mode--' ~ view_mode|clean_class,
+  not paragraph.isPublished() ? 'paragraph--unpublished',
+] %}
+{% block paragraph %}
+  <div{{ attributes.addClass(classes) }}>
+    {% block content %}
+      <div class=\"cards\">
+        <div class=\"container\">
+          {{ content.field_add_card }}
+        </div>
+      </div>
+    {% endblock %}
+  </div>
+{% endblock paragraph %}
+", "themes/custom/deposit_return_theme/templates/paragraphs/paragraph--cards.html.twig", "/var/www/html/web/themes/custom/deposit_return_theme/templates/paragraphs/paragraph--cards.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 1, "block" => 7);
+        static $filters = array("clean_class" => 3, "escape" => 8);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'block'],
+                ['clean_class', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
