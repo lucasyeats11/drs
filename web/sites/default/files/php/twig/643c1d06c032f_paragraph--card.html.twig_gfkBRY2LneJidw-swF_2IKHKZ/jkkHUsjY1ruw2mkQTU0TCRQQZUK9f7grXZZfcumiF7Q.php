b07bcca1bfef86ec8e7c5925407ffa33385e03f3
lucas <lucas@codewerks.co.uk>
2023-04-16 23:05:30 +0100

    styling tweaks

---
+++ web/sites/default/files/php/twig/643c1d06c032f_paragraph--card.html.twig_gfkBRY2LneJidw-swF_2IKHKZ/jkkHUsjY1ruw2mkQTU0TCRQQZUK9f7grXZZfcumiF7Q.php
@@ -0,0 +1,125 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/deposit_return_theme/templates/paragraphs/paragraph--card.html.twig */
+class __TwigTemplate_ff1c639322bb62f8c6cb0030de078a4b extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"card\">
+  <div class=\"image\">
+    ";
+        // line 3
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_image_upload", [], "any", false, false, true, 3), 3, $this->source), "html", null, true);
+        echo "
+  </div>
+  <div class=\"content-wrapper\">
+    <div class=\"content\">
+      <h3>";
+        // line 7
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_title", [], "any", false, false, true, 7), 7, $this->source), "html", null, true);
+        echo "</h3>
+      ";
+        // line 8
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_text", [], "any", false, false, true, 8), 8, $this->source), "html", null, true);
+        echo "
+      ";
+        // line 9
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_link", [], "any", false, false, true, 9), 9, $this->source), "html", null, true);
+        echo "
+    </div>
+  </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/deposit_return_theme/templates/paragraphs/paragraph--card.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  58 => 9,  54 => 8,  50 => 7,  43 => 3,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<div class=\"card\">
+  <div class=\"image\">
+    {{ content.field_image_upload }}
+  </div>
+  <div class=\"content-wrapper\">
+    <div class=\"content\">
+      <h3>{{ content.field_title }}</h3>
+      {{ content.field_text }}
+      {{ content.field_link }}
+    </div>
+  </div>
+</div>
+", "themes/custom/deposit_return_theme/templates/paragraphs/paragraph--card.html.twig", "/var/www/html/web/themes/custom/deposit_return_theme/templates/paragraphs/paragraph--card.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 3);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
